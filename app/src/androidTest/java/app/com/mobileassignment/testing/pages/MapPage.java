af35586d5fc110c8909fdcf922f092e2015120fe
Aya Akl <a.akl@mytaxi.com>
2020-05-21 01:48:26 +0200

    Add TODO for Map Page

--- app/src/androidTest/java/app/com/mobileassignment/testing/pages/MapPage.java
+++ app/src/androidTest/java/app/com/mobileassignment/testing/pages/MapPage.java
@@ -9,22 +9,14 @@ import static org.awaitility.Awaitility.await;
 
 public class MapPage extends BasePage {
 
-
+    //TODO: reuse these methods for checking if map is displayed or not
+    
     public MapPage waitForMapToBeDisplayed(ActivityTestRule activityTestRule) {
         await()
                 .atMost(30, SECONDS)
                 .until(isMapIntentFinishing(activityTestRule));
         return  this;
     }
-
-    /*
-    public MainPage waitForProgressBarToDisappear(ActivityTestRule activityTestRule) {
-        await()
-                .atMost(30, SECONDS)
-                .until(isProgressBarGone(activityTestRule));
-        return  this;
-    }
-     */
 
     //MARK: Private await methods
     private Callable<Boolean> isMapIntentFinishing(ActivityTestRule activityTestRule) {
